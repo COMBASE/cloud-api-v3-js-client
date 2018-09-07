c1a905accc000029009a6e25d6b935baf18b40f0
Philipp Dylong <phd@combase.de>
2018-09-07 15:19:22 +0200

    sh update

--- generate-api-code.sh
+++ generate-api-code.sh
@@ -1,5 +1,5 @@
-yarn global add swaggy-c;
-yarn add grunt grunt-cli grunt-bump;
+yarn global add swaggy-c
 swaggy-c --api-spec 'https://128.koronacloud.com/web/api/v3/swagger.yaml' --conf-file ./codegen-conf.json  --out-dir ./ javascript-gen
 yarn install
 yarn upgrade --latest
+yarn add --dev grunt grunt-cli grunt-bump
